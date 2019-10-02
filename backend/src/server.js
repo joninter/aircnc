ec15326a0201725b1424ba726b312b8a00a2540a
Jonatan <jonatan@iMac-de-Jonatan.local>
2019-10-02 16:01:11 -0300

    Backend Completo

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const routes = require('./routes');
 
 const app = express();
+
+mongoose.connect(
+  'mongodb+srv://joninter:c4l4r4d4@aircnc-fkmlj.mongodb.net/semana09?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
 app.use(express.json());
 app.use(routes);
